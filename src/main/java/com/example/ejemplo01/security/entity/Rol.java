ee14d35a7d5387b59b567ad0a2d17b3fa7fababc
Santiago Vivas <60908806+sagov8@users.noreply.github.com>
2021-06-04 00:05:34 -0500

    Cifrado funciona

---
+++ src/main/java/com/example/ejemplo01/security/entity/Rol.java
@@ -0,0 +1,41 @@
+package com.example.ejemplo01.security.entity;
+
+import com.example.ejemplo01.security.enums.RolNombre;
+import com.sun.istack.NotNull;
+
+import javax.persistence.Entity;
+import javax.persistence.*;
+
+
+@Entity
+public class Rol {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    private RolNombre rolNombre;
+
+    public Rol() {
+    }
+
+    public Rol(@NotNull RolNombre rolNombre) {
+        this.rolNombre = rolNombre;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public RolNombre getRolNombre() {
+        return rolNombre;
+    }
+
+    public void setRolNombre(RolNombre rolNombre) {
+        this.rolNombre = rolNombre;
+    }
+}
